da9c6b248db6b11e470ff83446d4ad7c4127e326
Beatriz Fernandes <93793289+Beatrizfernan@users.noreply.github.com>
2022-10-31 23:24:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio6_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor flutuante A:");
+            double A = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite o valor flutuante B:");
+            double B = double.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Digite o valor flutuante C:");
+            double C = double.Parse(Console.ReadLine());
+
+
+            double triangulo = A * C / 2.0;
+            double circulo = 3.14159 * C * C;
+            double trapezio = (A + B) / 2.0 * C;
+            double quadrado = B * B;
+            double retangulo = A * B;
+
+
+            Console.WriteLine("TRIANGULO:" + triangulo);
+            Console.WriteLine("CIRCULO: " + circulo);
+            Console.WriteLine("TRAPEZIO: " + trapezio);
+            Console.WriteLine("QUADRADO:  " + quadrado);
+            Console.WriteLine("RETANGULO: " + retangulo);
+          
+
+
+
+        }
+    }
+}
